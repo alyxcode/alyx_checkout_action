f76aba2d11d09d94617359b4030226e73c1267ca
Logan SQUIREL <logan.squirel@protonmail.com>
2021-04-22 09:31:20 +0200

    Refactoring src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,29 @@ const DEFAULT_BRANCH: &str = "default";
 const DEFAULT_REMOTE: &str = "origin";
 
 fn main() {
-    // git version
+    git_version();
+    git_init();
+    git_remote();
+    git_fetch();
+    git_checkout();
+    git_log();
+}
+
+fn git_version() {
     let command = "git --version";
     let output = exec(command);
-    stdout().write_all(&output.stdout).unwrap();
-    // initialize repository
+    stdout().write_all(&output.stdout).unwrap_or_else(|error| {
+        eprintln!("stdout error: {}", error);
+        exit(1);
+    });
+}
+
+fn git_init() {
     let command = format!("git init --initial-branch={}", DEFAULT_BRANCH);
     exec(command);
-    // add remote
+}
+
+fn git_remote() {
     let server = env("GITHUB_SERVER_URL");
     let repository = env("GITHUB_REPOSITORY");
     let command = format!(
@@ -23,7 +38,9 @@ fn main() {
         DEFAULT_REMOTE, server, repository
     );
     exec(command);
-    // fetch origin
+}
+
+fn git_fetch() {
     let refspec = env("GITHUB_REF");
     let command = format!(
         "git fetch \
@@ -34,13 +51,20 @@ fn main() {
         DEFAULT_REMOTE, refspec, DEFAULT_BRANCH
     );
     exec(command);
-    // checkout default branch
+}
+
+fn git_checkout() {
     let command = "git checkout";
     exec(command);
-    // show
+}
+
+fn git_log() {
     let command = "git --no-pager log --date=iso --no-decorate";
     let output = exec(command);
-    stdout().write_all(&output.stdout).unwrap();
+    stdout().write_all(&output.stdout).unwrap_or_else(|error| {
+        eprintln!("stdout error: {}", error);
+        exit(1);
+    });
 }
 
 fn exec<S: AsRef<str>>(command: S) -> Output {
@@ -52,31 +76,28 @@ fn exec<S: AsRef<str>>(command: S) -> Output {
     if output.status.success() {
         output
     } else {
-        eprintln!("command failed");
-        eprintln!("--- source ---");
-        eprintln!("{}", command);
         eprintln!("--- status ---");
         eprintln!("{}", output.status);
         eprintln!();
         eprintln!("--- stdout ---");
-        stderr().write_all(&output.stdout).unwrap();
+        stderr().write_all(&output.stdout).unwrap_or_else(|error| {
+            eprintln!("stderr error: {}", error);
+            exit(1);
+        });
         eprintln!();
         eprintln!("--- stderr ---");
-        stderr().write_all(&output.stderr).unwrap();
+        stderr().write_all(&output.stderr).unwrap_or_else(|error| {
+            eprintln!("stderr error: {}", error);
+            exit(1);
+        });
         eprintln!();
         exit(1);
     }
 }
 
 fn env(name: &str) -> String {
-    match var(name) {
-        Ok(var) => var,
-        Err(e) => {
-            eprintln!(
-                "failed to interpret environment variable \"{}\": {}",
-                name, e
-            );
-            exit(1);
-        }
-    }
+    var(name).unwrap_or_else(|error| {
+        eprintln!("environment variable error: {}", error);
+        exit(1);
+    })
 }
